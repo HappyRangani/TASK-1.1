5cb9fe74cab0cf7dd922293be43b6e068ad5c1bf
HappyRangani <happyrangani61@gmail.com>
2024-06-20 12:24:27 +0530

    Add files via upload

---
+++ task_1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+main()
+{
+	printf("Name: Happy Rangani\n");
+	printf("Age : 18\n");
+	printf("school: Monarch International School\n");
+	printf("hobby: cooking\n");
+}
---
+++ task_3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{
+	printf("---------\n");
+	printf("|\t|\n");
+	printf("H\t|\n");
+	printf("A\t|\n");
+	printf("p\t|\n");
+	printf("p\t|\n");
+	printf("y\t|\n");
+	printf("|\t|\n");
+	printf("|\t|\n");
+	printf("---------\n");
+}
